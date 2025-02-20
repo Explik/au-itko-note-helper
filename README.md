b199aded6ef8d9ec720fad924f2367198bb4d872
Explik <90565470+Explik@users.noreply.github.com>
2025-02-20 15:00:14 +0100

    Adding PDF to MD conversion proof of concept

---
+++ README.md
@@ -0,0 +1,4 @@
+# AU ITKO - Note Helper
+
+## Installation
+Run command ```pip install -r requirements.txt```
---
+++ src/convert-pdf-to-markdown.py
@@ -0,0 +1,27 @@
+from multiprocessing import freeze_support
+from marker.converters.pdf import PdfConverter
+from marker.models import create_model_dict
+from marker.output import text_from_rendered
+import os
+
+if __name__ == '__main__':
+    freeze_support()
+
+    converter = PdfConverter(
+        artifact_dict=create_model_dict(),
+    )
+    rendered = converter("slides\\1-software-udvikling.pdf")
+    text, _, image_map = text_from_rendered(rendered)
+
+    output_folder = "output"
+    os.makedirs(output_folder, exist_ok=True)
+
+    # Save text to a file
+    text_file_path = os.path.join(output_folder, "output.md")
+    with open(text_file_path, "w", encoding="utf-8") as text_file:
+        text_file.write(text)
+
+    # Save images to files
+    for image_name, image in image_map.items():
+        image_file_path = os.path.join(output_folder, image_name)
+        image.save(image_file_path)
