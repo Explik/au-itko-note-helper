eb8b0fd3da667f1065dca0c713fe3dbee4a383d8
Explik <90565470+Explik@users.noreply.github.com>
2025-02-22 11:01:13 +0100

    Moving rich text extraction into shared.py

--- src/convert-pdf-to-markdown.py
+++ src/convert-pdf-to-markdown.py
@@ -1,80 +1,9 @@
-from subprocess import Popen, PIPE, STDOUT
-import os
-from bs4 import BeautifulSoup
 
-def generate_html(file_path: str, output_dir: str):
-    if (file_path.endswith(".pdf") == False):
-        raise ValueError("The file must be a PDF file.")
 
-    # Run conversion command
-    command = [
-        "marker_single",
-        file_path,
-        "--output_format", "html",
-        "--output_dir", output_dir,
-        "--paginate_output"
-    ]
-    
-    process = Popen(command, shell=True, stdout=PIPE, stderr=STDOUT)
-    
-    with process.stdout:
-        for line in iter(process.stdout.readline, b''):
-            print(line.decode("utf-8").strip())
+from shared import extract_rich_text
 
-    result = process.wait()
-    
-    if result != 0:
-        print("An error occurred while converting the PDF to HTML.")
+file_path = ".\\slides\\1-software-udvikling.pdf"
+output_dir = ".\\output"
 
-def segment_html(file_path: str, output_dir: str):
-    # Find generated HTML file
-    file_name_without_ext = os.path.splitext(os.path.basename(file_path))[0]
-
-    html_folder_path = os.path.join(output_dir, file_name_without_ext)
-    html_file_path = os.path.join(html_folder_path, file_name_without_ext + ".html")
-    if (os.path.exists(html_file_path) == False):
-        raise ValueError("The HTML file does not exist.")
-    
-    # Read HTML file
-    with open(html_file_path, "r", encoding="utf-8") as file:
-        html_content = file.read()
-
-    # Split HTML content into pages
-    soup = BeautifulSoup(html_content, "html.parser")
-    pages = soup.find_all(class_="page")
-
-    for page in pages:
-        page_id = page.get("data-page-id")
-        if page_id:
-
-            # Create new HTML file
-            output_html_file_path = os.path.join(html_folder_path, f"page_{page_id}.html")
-            with open(output_html_file_path, "w", encoding="utf-8") as output_file:
-                for child in page.children:
-                    output_file.write(str(child))
-
-            output_text_file_path = os.path.join(html_folder_path, f"page_{page_id}.txt")
-            with open(output_text_file_path, "w", encoding="utf-8") as output_file:
-                page_text = page.get_text().replace("  ", " ").strip()
-                output_file.write(page_text)
-
-def rename_output_folder(file_path: str, output_dir: str):
-    # Find generated HTML file
-    file_name_without_ext = os.path.splitext(os.path.basename(file_path))[0]
-
-    html_folder_path = os.path.join(output_dir, file_name_without_ext)
-    if (os.path.exists(html_folder_path) == False):
-        raise ValueError("The HTML folder does not exist.")
-    
-    # Rename output folder
-    new_html_folder_path = os.path.join(output_dir, "html")
-    os.rename(html_folder_path, new_html_folder_path)
-
-if __name__ == "__main__":
-    file_path = ".\\slides\\1-software-udvikling.pdf"
-    output_dir = ".\\output"
-
-    generate_html(file_path, output_dir)
-    segment_html(file_path, output_dir)
-    rename_output_folder(file_path, output_dir)
-    
+pages = extract_rich_text(file_path, output_dir)
+print(pages)
--- src/shared.py
+++ src/shared.py
@@ -1,12 +1,37 @@
 # import module
 from pdf2image import convert_from_path
 from pypdf import PdfReader
+from subprocess import Popen, PIPE, STDOUT
+from bs4 import BeautifulSoup
 import os
 
+# Utility functions
 def create_dir_if_not_exists(dir_path):
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
 
+def delete_dir_if_exists(dir_path):
+    assert dir_path not in ['/', '\\'], "Cannot delete root directory."
+    assert os.path.isdir(dir_path), f"{dir_path} is not a directory."
+
+    if os.path.exists(dir_path):
+        for root, dirs, files in os.walk(dir_path):
+            for file in files:
+                os.remove(os.path.join(root, file))
+            for dir in dirs:
+                os.rmdir(os.path.join(root, dir))
+        os.rmdir(dir_path)
+
+def run_command(command):
+    process = Popen(command, shell=True, stdout=PIPE, stderr=STDOUT)
+    
+    with process.stdout:
+        for line in iter(process.stdout.readline, b''):
+            print(line.decode("utf-8").strip())
+
+    return process.wait()
+
+# PDF extraction functions
 def extract_screenshots(pdf_file_path, output_dir): 
     # Store Pdf with convert_from_path function
     images = convert_from_path(pdf_file_path)
@@ -21,7 +46,7 @@ def extract_screenshots(pdf_file_path, output_dir):
         images[i].save(image_file_name, 'JPEG')
 
         buffer.append(image_file_name)
-    
+
     return buffer
 
 def extract_plain_text(pdf_file_path, output_dir): 
@@ -44,4 +69,70 @@ def extract_plain_text(pdf_file_path, output_dir):
     
     return buffer
 
+def extract_rich_text(pdf_file_path, output_dir): 
+    html_file_name = generate_single_html_file(pdf_file_path, output_dir)
+    html_file_names = split_single_html_file(html_file_name, output_dir)
+    
+    return html_file_names
 
+def generate_single_html_file(pdf_file_path: str, output_dir: str):
+    if (pdf_file_path.endswith(".pdf") == False):
+        raise ValueError("The file must be a PDF file.")
+
+    # Create output directory if it doesn't exist
+    create_dir_if_not_exists(output_dir)
+
+    # Run conversion command
+    command_status = run_command([
+        "marker_single",
+        pdf_file_path,
+        "--output_format", "html",
+        "--output_dir", output_dir,
+        "--paginate_output"
+    ])
+
+    if command_status != 0:
+        raise ValueError("An error occurred while converting the PDF to HTML.", command_status)
+
+    # Rename output folder to "html" instead of file name without extension (overwrite any existing folder)
+    file_name_without_ext = os.path.splitext(os.path.basename(pdf_file_path))[0]
+    html_folder_path = os.path.join(output_dir, file_name_without_ext)
+    new_html_folder_path = os.path.join(output_dir, "html")
+    
+    delete_dir_if_exists(new_html_folder_path)
+
+    os.rename(html_folder_path, new_html_folder_path)
+
+    # Return HTML file path
+    return os.path.join(new_html_folder_path, file_name_without_ext + ".html")
+
+def split_single_html_file(html_file_path: str, output_dir: str):
+    if (os.path.exists(html_file_path) == False):
+        raise ValueError("The HTML file does not exist.")
+    
+    # Read HTML file
+    with open(html_file_path, "r", encoding="utf-8") as file:
+        html_content = file.read()
+
+    # Split HTML content into pages
+    soup = BeautifulSoup(html_content, "html.parser")
+    pages = soup.find_all(class_="page")
+
+    buffer = []
+    for page in pages:
+        page_id = page.get("data-page-id")
+        if page_id:
+            # Create new HTML file
+            output_html_file_path = os.path.join(output_dir, f"page_{page_id}.html")
+            with open(output_html_file_path, "w", encoding="utf-8") as output_file:
+                for child in page.children:
+                    output_file.write(str(child))
+            buffer.append(output_html_file_path)
+
+            # Create new text file
+            output_text_file_path = os.path.join(output_dir, f"page_{page_id}.txt")
+            with open(output_text_file_path, "w", encoding="utf-8") as output_file:
+                page_text = page.get_text().replace("  ", " ").strip()
+                output_file.write(page_text)
+
+    return buffer
